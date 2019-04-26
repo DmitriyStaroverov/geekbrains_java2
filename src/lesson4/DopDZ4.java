b1f3f6c127f5e3d3f9bbe1dea28611671dcadbfa
Dmitriy Staroverov <star492@mail.ru>
2019-04-26 14:38:52 +0400

    dop dz 4 доделал

--- src/lesson4/DopDZ4.java
+++ src/lesson4/DopDZ4.java
@@ -7,63 +7,69 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
-public class DopDZ4 extends JFrame {
-
-    JTextField jtf1;
-    JTextField jtf2;
-    JTextField jtf3;
+public class DopDZ4 {
 
     public static void main(String[] args) {
-        new DopDZ4 ().setVisible ( true );
-    }
+        new MyWind ();
 
-    DopDZ4() {
-        setTitle ( "DopDZ4" );
-        setBounds ( 800, 300, 400, 400 );
-
-        ImageIcon icon = new ImageIcon ( "src/img/hashtag.png" );
-        setIconImage ( icon.getImage () );
-        setDefaultCloseOperation ( WindowConstants.EXIT_ON_CLOSE );
-
-        setLayout ( new GridLayout ( 4,1, 20, 15) );
-
-        jtf1 = new JTextField ( );
-        jtf2 = new JTextField ( );
-        jtf3 = new JTextField ( );
-        jtf1.setPreferredSize ( new Dimension ( 300,28 ) );
-        jtf2.setPreferredSize ( new Dimension ( 300,28 ) );
-        jtf3.setPreferredSize ( new Dimension ( 300,28 ) );
-        jtf1.setBorder ( BorderFactory.createTitledBorder ( "1" ) );
-        jtf2.setBorder ( BorderFactory.createTitledBorder ( "2" ) );
-        jtf3.setBorder ( BorderFactory.createTitledBorder ( "3" ) );
-        jtf1.setEditable ( false );
-        jtf2.setEditable ( false );
-        jtf3.setEditable ( false );
-        add ( jtf1 );
-        add ( jtf2 );
-        add ( jtf3 );
-
-        JButton jbtSend = new JButton ( "Send" );
-
-        add(jbtSend);
-
-        jbtSend.addActionListener ( new ActionListener () {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                setEnabled ( false );
-                new DopForm ().setLocation ( getX ()+10, getY ()+10 );
-            }
-        } );
     }
 
 }
 
-class DopForm extends DopDZ4{
-    public DopForm() {
-        super();
+class MyWind extends JFrame {
+
+    public JLabel jlName;
+
+    public JLabel jlSurName;
+
+    public MyWind() {
+        setTitle ( "DopDZ4" );
+        setBounds ( 800,300,400,400 );
+        setDefaultCloseOperation ( WindowConstants.EXIT_ON_CLOSE );
+        JPanel jPanel = new JPanel ( new GridLayout ( 1,2 ) );
+        jlName = new JLabel ( "Name" );
+        jlSurName = new JLabel ( "SurName" );
+        jPanel.add ( jlName );
+        jPanel.add ( jlSurName );
+        add ( jPanel,BorderLayout.NORTH );
+        JButton jButton = new JButton ( "Send" );
+        DopForm dopForm = new DopForm ( this );
+        jButton.addActionListener ( new ActionListener () {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dopForm.setVisible ( true );
+            }
+        } );
+        add ( jButton,BorderLayout.SOUTH );
         setVisible ( true );
-        setDefaultCloseOperation ( WindowConstants.HIDE_ON_CLOSE );
 
+    }
+}
 
+class DopForm extends JFrame{
+
+    JFrame parentFrame;
+
+    public DopForm(MyWind parentFrame) {
+        this.parentFrame = parentFrame;
+        setTitle ( "DopDZ4" );
+        setBounds ( 800,300,400,400 );
+        setDefaultCloseOperation ( WindowConstants.EXIT_ON_CLOSE );
+        JPanel jPanel = new JPanel ( new GridLayout ( 1,2 ) );
+        JTextField jTextField = new JTextField (  );
+        JTextField jTextField1= new JTextField (  );
+        jPanel.add ( jTextField );
+        jPanel.add ( jTextField1 );
+        add ( jPanel,BorderLayout.NORTH );
+        JButton jButton = new JButton ( "Send2" );
+        jButton.addActionListener ( new ActionListener () {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                parentFrame.jlName.setText ( jTextField.getText () );
+                parentFrame.jlSurName.setText ( jTextField1.getText () );
+                setVisible ( false );
+            }
+        } );
+        add ( jButton,BorderLayout.SOUTH );
     }
 }
